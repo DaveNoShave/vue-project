75976fd8c2f8811edb1a043a9924761e8de5c855
DaveNoShave <125213084+DaveNoShave@users.noreply.github.com>
2023-05-09 13:31:24 +0200

    1 binaries uploaded

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,34 @@
 <template>
-<body>
-  <div class="login-page">
-  <img src="bilen.png" alt="Bilen">
-    <h1>Login</h1>
-    <form @submit.prevent="login">
-      <label for="email">Email:</label>
-      <input type="email" id="email" v-model="email" required />
+  <body>
+    <div class="login-page">
+      <img src="bilen.png" alt="Bilen" />
+      <h1>Login</h1>
+      <form @submit.prevent="login">
+        <label for="email">Email:</label>
+        <input type="email" id="email" v-model="email" required />
 
-      <label for="password">Password:</label>
-      <input type="password" id="password" v-model="password" required />
+        <label for="password">Password:</label>
+        <input type="password" id="password" v-model="password" required />
 
-      <div>
-        <label for="rememberMe">Remember me:</label>
-        <input type="checkbox" id="rememberMe" v-model="rememberMe" />
-      </div>
+        <div>
+          <label for="rememberMe">Remember me:</label>
+          <input type="checkbox" id="rememberMe" v-model="rememberMe" />
+        </div>
 
-      <button type="submit">Login</button>
+        <button type="submit">Login</button>
 
-      <button
-        type="button"
-        @click="forgotPassword"
-        class="forgot-password-button"
-      >
-        Forgot Password
-      </button>
-    </form>
-    <p v-if="error" class="error-message">{{ error }}</p>
-  </div>
+        <button
+          type="button"
+          @click="forgotPassword"
+          class="forgot-password-button"
+        >
+          Forgot Password
+        </button>
+      </form>
+      <p v-if="error" class="error-message">{{ error }}</p>
+    </div>
   </body>
 </template>
-
 
 <style>
 body {
